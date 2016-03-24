93d4635ceae62ec9d126008f1c5169c8c2e94335
name <email>
2016-03-24 18:03:28 +0200

    CheckStyle SAP

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,29 +1,10 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.function.Function;
 
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
-/*Corner cases.  
- * All methods and the constructor should throw a java.lang.NullPointerException 
- * if any argument is null. 
- * The constructor should throw a java.lang.IllegalArgumentException if the 
- * input does not correspond to a rooted DAG. 
- * The distance() and sap() methods should throw a 
- * java.lang.IllegalArgumentException unless both of the noun arguments 
- * are WordNet nouns.
-
- Performance requirements.  
- Your data type should use space linear in the input size 
- (size of synsets and hypernyms files). 
- The constructor should take time linearithmic (or better) in the input size. 
- The method isNoun() should run in time logarithmic (or better) in the number of nouns. 
- The methods distance() and sap() should run in time linear in the size of the WordNet digraph. 
- For the analysis, assume that the number of nouns per synset is bounded by a constant. */
 public class WordNet {
 
 	// constructor takes the name of the two input files
@@ -31,9 +12,6 @@ public class WordNet {
 		if (isNullOrEmpty(synsets) || isNullOrEmpty(hypernyms)) {
 			throw new java.lang.NullPointerException();
 		}
-
-		// The constructor should take time linearithmic (or better) in the
-		// input size.
 
 		buildNounData(synsets);
 		buildGraph(hypernyms);
@@ -56,36 +34,17 @@ public class WordNet {
 
 	// is the word a WordNet noun?
 	public boolean isNoun(String word) {
-		// The method isNoun() should run in time logarithmic (or better) in the
-		// number of nouns.
-
 		return mapNoun2ID.containsKey(word);
 	}
 
 	// distance between nounA and nounB (defined below)
 	public int distance(String nounA, String nounB) {
-		/*
-		 * The distance() method should throw a
-		 * java.lang.IllegalArgumentException unless both of the noun arguments
-		 * are WordNet nouns.
-		 */
 		Function3<Iterable<Integer>, Iterable<Integer>, Integer> f = (v, w) -> sap
 				.length(v, w);
 		return searchWordNet(nounA, nounB, f);
 	}
 
-	// a synset (second field of synsets.txt) that is the common ancestor of
-	// nounA and nounB
-	// in a shortest ancestral path (defined below)
 	public String sap(String nounA, String nounB) {
-		/*
-		 * The sap() method should throw a java.lang.IllegalArgumentException
-		 * unless both of the noun arguments are WordNet nouns.
-		 */
-
-		// The methods distance() and sap() should run in time linear in the
-		// size of the WordNet digraph.
-
 		Function3<Iterable<Integer>, Iterable<Integer>, Integer> f = (v, w) -> sap
 				.ancestor(v, w);
 		int ancestor = searchWordNet(nounA, nounB, f);
@@ -97,9 +56,6 @@ public class WordNet {
 		if (!isNoun(nounA) || !isNoun(nounB)) {
 			throw new java.lang.IllegalArgumentException();
 		}
-
-		// The methods distance() and sap() should run in time linear in the
-		// size of the WordNet digraph.
 
 		Iterable<Integer> ai = mapNoun2ID.get(nounA);
 		Iterable<Integer> bi = mapNoun2ID.get(nounB);
